5bef8c8b77da10fd73d273e890ca149f01d57108
Jose Luis Quintana <joseluisquintana20@gmail.com>
2015-07-30 00:00:13 -0500

    Fixed coveralls setup

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function (config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'src/*.js': ['coverage']
+      'lib/*.js': ['coverage']
     },
 
     // optionally, configure the reporter
